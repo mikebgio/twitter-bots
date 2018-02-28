953a18a7642df807004a32c28e726ec0d0c960c0
Michael Giordano <mgiordano@Michaels-iMac.local>
2018-02-28 00:01:05 -0500

    Added requirements and cleaned up trump_listen.py

--- trump_listen.py
+++ trump_listen.py
@@ -10,32 +10,8 @@ auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
 auth.set_access_token(settings.ACCESS_TOKEN, settings.ACCESS_SECRET)
 api = tweepy.API(auth)
 print(api)
-TRUMP_ID = '25073877'
 
 db = dataset.connect(settings.CONNECTION_STRING)
-
-# SQLITE
-# TABLE = 'trumps'  # TABLE IN tweets.db
-# DB = 'tweets.db' # HAS table trumps
-
-### TESTING STUFF ###
-# TABLE = 'tweets'. #TEST
-# DB = 'trump.db' #TEST
-
-# conn = sqlite3.connect(DB)
-# curs = conn.cursor()
-
-
-
-# def add_row(tweet):
-#     if tweet.is_quote_status:
-#         quote_status = tweet.quoted_status_id
-#     else:
-#         quote_status = None
-#     curs.execute("INSERT OR IGNORE INTO {tn} values((?),(?),(?),(?),(?),(?),(?),(?))".format(tn=TABLE), (tweet.id,
-#         str(tweet.created_at.date()), str(tweet.created_at.time()), tweet.text, tweet.retweet_count, tweet.favorite_count,
-#         tweet.in_reply_to_status_id, quote_status))
-#     conn.commit()
 
 
 class StreamListener(tweepy.StreamListener):
@@ -58,19 +34,17 @@ class StreamListener(tweepy.StreamListener):
         table = db[settings.TABLE_NAME]
         try:
             table.insert_ignore(dict(
-                    id=tweet_id,
-                    date=date,
-                    time=time,
-                    text=text,
-                    retweets=retweets,
-                    favorites=favorites,
-                    reply_to=reply_to,
-                    quote_status=quote_status
-                ))
+                id=tweet_id,
+                date=date,
+                time=time,
+                text=text,
+                retweets=retweets,
+                favorites=favorites,
+                reply_to=reply_to,
+                quote_status=quote_status
+            ))
         except ProgrammingError as err:
             print(err)
-
-        # add_row(status)
 
     def on_error(self, status_code):
         if status_code == 420:
@@ -80,6 +54,5 @@ class StreamListener(tweepy.StreamListener):
 
 
 trumpStreamListener = StreamListener()
-stream = tweepy.Stream(auth = api.auth, listener=trumpStreamListener)
-#stream.filter(follow=[TRUMP_ID])
-stream.filter(track=settings.TRACK_TERMS)
+stream = tweepy.Stream(auth=api.auth, listener=trumpStreamListener)
+stream.filter(follow=[settings.TRUMP_ID])
